e3a370632c240b61bb31635c5c26096b10cedf82
Tao Huang <89457786+TaoHuang0@users.noreply.github.com>
2022-11-28 13:48:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
   const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    
     res.end(data);
   });
   
