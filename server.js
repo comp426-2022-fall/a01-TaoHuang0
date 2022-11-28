a1472951b87e9e5b9ab51342e695f355e74769b0
Tao Huang <89457786+TaoHuang0@users.noreply.github.com>
2022-11-28 13:46:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,13 +32,3 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
     console.log(`Server listening on port ${port}`);
   });
 });
-
-
-
-
-// Start the `server` const listening on the port defined by argument in your `port` const. 
-// Put the exact message `Server listening on port ${port}` on the console log.
-
-
-
-// That's it! You're all done!
