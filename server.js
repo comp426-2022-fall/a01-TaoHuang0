60060cbecfe54232025423e95a21f8074ddf92be
Tao Huang <89457786+TaoHuang0@users.noreply.github.com>
2022-11-28 13:50:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
   })
   
   server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`)
   });
 });
